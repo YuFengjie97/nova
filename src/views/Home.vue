4e6c135d8060358dc9b61894e5bd913751b17d39
YuFengjie97 <2457522528@qq.com>
2022-10-20 22:46:10 +0800

    feat: add vueRouter less viteConfigChange

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <router-link to="/">home</router-link>
+    <router-link to="about">about</router-link>
+    <router-view/>
+  </div>
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const msg = ref('hello')
+</script>
+<style lang="less" scoped>
+.home {
+}
+</style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class='notFound'>
+    <h1>NOT FOUND</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref} from 'vue';
+
+</script>
+
+<style lang='less' scoped>
+  .notFound{
+    display: flex;
+    justify-content: center;
+    align-items: center;  
+  }
+</style>
