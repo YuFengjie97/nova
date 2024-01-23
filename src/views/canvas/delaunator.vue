e2ddf6c7e5c5014e6e4bf7e3359e2474e09d12b8
yufengjie <2457522528@qq.com>
2024-01-23 14:00:02 +0800

    delaunator

--- src/views/canvas/delaunator.vue
+++ src/views/canvas/delaunator.vue
@@ -2,17 +2,17 @@
 import { onMounted, ref } from 'vue'
 import Delaunator from 'delaunator'
 import { createNoise2D } from 'simplex-noise'
+import { getTriangleCenter } from '@/utils/math'
 import { type Vec3, initPalette, vec3ToRgb } from '@/utils/color'
 
 const noise = createNoise2D()
-const { random } = Math
+const { random, sin } = Math
 const canvas = ref<HTMLCanvasElement>()
 
-const a: Vec3 = [0.000, 0.500, 0.500]
-const b: Vec3 = [0.000, 0.500, 0.500]
-const c: Vec3 = [0.000, 0.333, 0.500]
-const d: Vec3 = [0.000, 0.667, 0.500]
-
+const a: Vec3 = [1.000, 0.500, 0.500]
+const b: Vec3 = [0.500, 0.500, 0.500]
+const c: Vec3 = [0.750, 1.000, 0.667]
+const d: Vec3 = [0.800, 1.000, 0.333]
 const palette = initPalette(a, b, c, d)
 
 onMounted(() => {
@@ -24,10 +24,17 @@ onMounted(() => {
   ctx.strokeStyle = 'transparent'
   canvas.value.width = width
   canvas.value.height = height
-  const total = 100
+  const total = 300
 
-  const trianglePoints = initTrianglePoints()
+  interface Point { x: number, y: number }
+  const trianglePoints: Array<{
+    p1: Point
+    p2: Point
+    p3: Point
+  }> = []
+  const triangleCenter: Array<Point> = []
 
+  initTrianglePoints()
   function initTrianglePoints() {
     const coords = []
     for (let i = 0; i < total; i += 1) {
@@ -38,7 +45,6 @@ onMounted(() => {
     const delaunay = new Delaunator(coords)
     const triangles = delaunay.triangles // triangles代表三角形顶点坐标在coords中的索引，三个一组
 
-    const trianglePoints = []
     for (let i = 0; i < triangles.length; i += 3) {
       const i1 = triangles[i]
       const i2 = triangles[i + 1]
@@ -47,24 +53,30 @@ onMounted(() => {
       const p1 = { x: coords[i1 * 2], y: coords[i1 * 2 + 1] }
       const p2 = { x: coords[i2 * 2], y: coords[i2 * 2 + 1] }
       const p3 = { x: coords[i3 * 2], y: coords[i3 * 2 + 1] }
+
+      const center = getTriangleCenter(p1, p2, p3)
+      triangleCenter.push(center)
       trianglePoints.push({ p1, p2, p3 })
     }
-    return trianglePoints
   }
 
   function animate() {
-    const t = performance.now() / 1000
+    let t = performance.now() / 5000
+    t = sin(t) + 1
     // ctx.fillStyle = 'rgba(0,0,0,0.2)'
     ctx.fillStyle = '#000'
     ctx.fillRect(0, 0, width, height)
 
-    trianglePoints.forEach(({ p1, p2, p3 }) => {
+    trianglePoints.forEach(({ p1, p2, p3 }, i) => {
       ctx.beginPath()
       ctx.moveTo(p1.x, p1.y)
       ctx.lineTo(p2.x, p2.y)
       ctx.lineTo(p3.x, p3.y)
       ctx.lineTo(p1.x, p1.y)
-      ctx.fillStyle = `${vec3ToRgb(palette(noise(p1.x + t, p1.y + t)))}`
+      const color = vec3ToRgb(palette(noise(triangleCenter[i].x + t, triangleCenter[i].y + t)))
+      ctx.strokeStyle = color
+      ctx.fillStyle = color
+      ctx.stroke()
       ctx.fill()
     })
 
