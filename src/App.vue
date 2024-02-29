0cfec88ea27c269c67b775e32794eeecbc614902
yufengjie <2457522528@qq.com>
2024-02-29 14:41:03 +0800

    github link

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <script setup lang="ts"></script>
 
 <template>
-  <router-view></router-view>
+  <div class="relative w-full h-full overflow-hidden">
+    <div class="github absolute bottom-0 right-0 z-900 w-300px h-28px bg-#fff flex justify-center items-center">
+      <a href="https://github.com/YuFengjie97/nova" target="_blank">
+        Star Me
+        <img src="@/assets/img/github.png" class="w-20px h-20px m-l-10px" alt="">
+      </a>
+    </div>
+    <router-view />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="less">
+.github{
+  transform-origin: center;
+  transform:translate(33%, -100%) rotate(-45deg) ;
+  box-shadow: 0 0 10px #eee;
+  a {
+    color: #000;
+    text-decoration: none;
+  }
+}
+</style>
