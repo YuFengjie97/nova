783b0bcbb80bcbca533d62f5a5dbfc3841ba3e1b
yufengjie <2457522528@qq.com>
2024-03-08 09:48:36 +0800

    fuck star

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
 <template>
   <div class="relative w-full h-full overflow-hidden">
     <div class="github absolute bottom-0 right-0 z-900 w-300px h-28px bg-#fff flex justify-center items-center">
-      <a href="https://github.com/YuFengjie97/nova" target="_blank">
-        Star Me
-        <img src="@/assets/img/github.png" class="w-20px h-20px m-l-10px" alt="">
+      <a class="flex items-center" href="https://github.com/YuFengjie97/nova" target="_blank">
+        DON'T Star Me
+        <img src="@/assets/img/github.png" class="w-20px h-20px m-l-4px" alt="">
       </a>
     </div>
     <router-view />
@@ -15,7 +15,7 @@
 <style scoped lang="less">
 .github{
   transform-origin: center;
-  transform:translate(33%, -100%) rotate(-45deg) ;
+  transform:translate(33%, -150%) rotate(-45deg) ;
   box-shadow: 0 0 2px #eee;
   transition: box-shadow .3s;
   a {
