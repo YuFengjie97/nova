17b9ff0e28f6a634d83131be8e8ce3d76988fd9e
yufengjie <2457522528@qq.com>
2024-04-09 10:08:32 +0800

    build

--- docs/assets/disco_communism-NN-qA4tW.js
+++
@@ -1,1 +0,0 @@
-const s="/nova/assets/disco_communism-8paHOis6.jpg";export{s as p};
---
+++ docs/assets/initGUI-CcmszmDW.js
@@ -0,0 +1,1 @@
+import{G as o}from"./dat.gui.module-DcMWPYuf.js";function i(t){const e=new o({autoPlace:!1});return t.append(e.domElement),e.domElement.style.cssText="position: fixed; top: 0; right: 0;",{gui:e}}export{i};
