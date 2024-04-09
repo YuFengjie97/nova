ae46d7a7ea3b7295d0e45ffece2df7073bf52b3a
yufengjie <2457522528@qq.com>
2024-04-09 10:26:52 +0800

    build

---
+++ docs/assets/LensFlares-tdm2qu8q.js
@@ -0,0 +1,106 @@
+import{G as oe}from"./dat.gui.module-DcMWPYuf.js";import{al as ie,G as R,ao as N,M as B,au as ne,aN as X,aO as Y,aP as I,C as W,V as Q,K as se,aQ as ae,L as re,aR as ce,aS as J,aT as le,d as he,r as O,h as ue,H as de,X as Z,aj as me,aL as ve,S as pe,av as fe,P as we,W as ye,B as be,b as ge,e as $,aJ as Se,o as xe,_ as ke}from"./index-DzmhKhXa.js";import{S as Ve}from"./stats.module--VATS4Kh.js";import{F as Le,f as Ee,T as Me}from"./helvetiker_regular.typeface-DGeVbZJz.js";const Pe={type:"change"};class De extends ie{constructor(i,c){super(),this.object=i,this.domElement=c,this.movementSpeed=1,this.rollSpeed=.005,this.dragToLook=!1,this.autoForward=!1;const e=this,y=1e-6,b=new N,S=new R;this.tmpQuaternion=new N,this.status=0,this.moveState={up:0,down:0,left:0,right:0,forward:0,back:0,pitchUp:0,pitchDown:0,yawLeft:0,yawRight:0,rollLeft:0,rollRight:0},this.moveVector=new R(0,0,0),this.rotationVector=new R(0,0,0),this.keydown=function(o){if(!o.altKey){switch(o.code){case"ShiftLeft":case"ShiftRight":this.movementSpeedMultiplier=.1;break;case"KeyW":this.moveState.forward=1;break;case"KeyS":this.moveState.back=1;break;case"KeyA":this.moveState.left=1;break;case"KeyD":this.moveState.right=1;break;case"KeyR":this.moveState.up=1;break;case"KeyF":this.moveState.down=1;break;case"ArrowUp":this.moveState.pitchUp=1;break;case"ArrowDown":this.moveState.pitchDown=1;break;case"ArrowLeft":this.moveState.yawLeft=1;break;case"ArrowRight":this.moveState.yawRight=1;break;case"KeyQ":this.moveState.rollLeft=1;break;case"KeyE":this.moveState.rollRight=1;break}this.updateMovementVector(),this.updateRotationVector()}},this.keyup=function(o){switch(o.code){case"ShiftLeft":case"ShiftRight":this.movementSpeedMultiplier=1;break;case"KeyW":this.moveState.forward=0;break;case"KeyS":this.moveState.back=0;break;case"KeyA":this.moveState.left=0;break;case"KeyD":this.moveState.right=0;break;case"KeyR":this.moveState.up=0;break;case"KeyF":this.moveState.down=0;break;case"ArrowUp":this.moveState.pitchUp=0;break;case"ArrowDown":this.moveState.pitchDown=0;break;case"ArrowLeft":this.moveState.yawLeft=0;break;case"ArrowRight":this.moveState.yawRight=0;break;case"KeyQ":this.moveState.rollLeft=0;break;case"KeyE":this.moveState.rollRight=0;break}this.updateMovementVector(),this.updateRotationVector()},this.pointerdown=function(o){if(this.dragToLook)this.status++;else{switch(o.button){case 0:this.moveState.forward=1;break;case 2:this.moveState.back=1;break}this.updateMovementVector()}},this.pointermove=function(o){if(!this.dragToLook||this.status>0){const r=this.getContainerDimensions(),n=r.size[0]/2,u=r.size[1]/2;this.moveState.yawLeft=-(o.pageX-r.offset[0]-n)/n,this.moveState.pitchDown=(o.pageY-r.offset[1]-u)/u,this.updateRotationVector()}},this.pointerup=function(o){if(this.dragToLook)this.status--,this.moveState.yawLeft=this.moveState.pitchDown=0;else{switch(o.button){case 0:this.moveState.forward=0;break;case 2:this.moveState.back=0;break}this.updateMovementVector()}this.updateRotationVector()},this.update=function(o){const r=o*e.movementSpeed,n=o*e.rollSpeed;e.object.translateX(e.moveVector.x*r),e.object.translateY(e.moveVector.y*r),e.object.translateZ(e.moveVector.z*r),e.tmpQuaternion.set(e.rotationVector.x*n,e.rotationVector.y*n,e.rotationVector.z*n,1).normalize(),e.object.quaternion.multiply(e.tmpQuaternion),(S.distanceToSquared(e.object.position)>y||8*(1-b.dot(e.object.quaternion))>y)&&(e.dispatchEvent(Pe),b.copy(e.object.quaternion),S.copy(e.object.position))},this.updateMovementVector=function(){const o=this.moveState.forward||this.autoForward&&!this.moveState.back?1:0;this.moveVector.x=-this.moveState.left+this.moveState.right,this.moveVector.y=-this.moveState.down+this.moveState.up,this.moveVector.z=-o+this.moveState.back},this.updateRotationVector=function(){this.rotationVector.x=-this.moveState.pitchDown+this.moveState.pitchUp,this.rotationVector.y=-this.moveState.yawRight+this.moveState.yawLeft,this.rotationVector.z=-this.moveState.rollRight+this.moveState.rollLeft},this.getContainerDimensions=function(){return this.domElement!=document?{size:[this.domElement.offsetWidth,this.domElement.offsetHeight],offset:[this.domElement.offsetLeft,this.domElement.offsetTop]}:{size:[window.innerWidth,window.innerHeight],offset:[0,0]}},this.dispose=function(){this.domElement.removeEventListener("contextmenu",ee),this.domElement.removeEventListener("pointerdown",F),this.domElement.removeEventListener("pointermove",k),this.domElement.removeEventListener("pointerup",p),window.removeEventListener("keydown",x),window.removeEventListener("keyup",h)};const k=this.pointermove.bind(this),F=this.pointerdown.bind(this),p=this.pointerup.bind(this),x=this.keydown.bind(this),h=this.keyup.bind(this);this.domElement.addEventListener("contextmenu",ee),this.domElement.addEventListener("pointerdown",F),this.domElement.addEventListener("pointermove",k),this.domElement.addEventListener("pointerup",p),window.addEventListener("keydown",x),window.addEventListener("keyup",h),this.updateMovementVector(),this.updateRotationVector()}}function ee(w){w.preventDefault()}class U extends B{constructor(){super(U.Geometry,new ne({opacity:0,transparent:!0})),this.isLensflare=!0,this.type="Lensflare",this.frustumCulled=!1,this.renderOrder=1/0;const i=new R,c=new R,e=new X(16,16,Y),y=new X(16,16,Y),b=U.Geometry,S=new I({uniforms:{scale:{value:null},screenPosition:{value:null}},vertexShader:`
+
+				precision highp float;
+
+				uniform vec3 screenPosition;
+				uniform vec2 scale;
+
+				attribute vec3 position;
+
+				void main() {
+
+					gl_Position = vec4( position.xy * scale + screenPosition.xy, screenPosition.z, 1.0 );
+
+				}`,fragmentShader:`
+
+				precision highp float;
+
+				void main() {
+
+					gl_FragColor = vec4( 1.0, 0.0, 1.0, 1.0 );
+
+				}`,depthTest:!0,depthWrite:!1,transparent:!1}),k=new I({uniforms:{map:{value:e},scale:{value:null},screenPosition:{value:null}},vertexShader:`
+
+				precision highp float;
+
+				uniform vec3 screenPosition;
+				uniform vec2 scale;
+
+				attribute vec3 position;
+				attribute vec2 uv;
+
+				varying vec2 vUV;
+
+				void main() {
+
+					vUV = uv;
+
+					gl_Position = vec4( position.xy * scale + screenPosition.xy, screenPosition.z, 1.0 );
+
+				}`,fragmentShader:`
+
+				precision highp float;
+
+				uniform sampler2D map;
+
+				varying vec2 vUV;
+
+				void main() {
+
+					gl_FragColor = texture2D( map, vUV );
+
+				}`,depthTest:!1,depthWrite:!1,transparent:!1}),F=new B(b,S),p=[],x=D.Shader,h=new I({uniforms:{map:{value:null},occlusionMap:{value:y},color:{value:new W(16777215)},scale:{value:new Q},screenPosition:{value:new R}},vertexShader:x.vertexShader,fragmentShader:x.fragmentShader,blending:se,transparent:!0,depthWrite:!1}),o=new B(b,h);this.addElement=function(s){p.push(s)};const r=new Q,n=new Q,u=new ae,t=new le;this.onBeforeRender=function(s,_,d){s.getCurrentViewport(t);const g=t.w/t.z,A=t.z/2,v=t.w/2;let V=16/t.w;if(r.set(V*g,V),u.min.set(t.x,t.y),u.max.set(t.x+(t.z-16),t.y+(t.w-16)),c.setFromMatrixPosition(this.matrixWorld),c.applyMatrix4(d.matrixWorldInverse),!(c.z>0)&&(i.copy(c).applyMatrix4(d.projectionMatrix),n.x=t.x+i.x*A+A-8,n.y=t.y+i.y*v+v-8,u.containsPoint(n))){s.copyFramebufferToTexture(n,e);let L=S.uniforms;L.scale.value=r,L.screenPosition.value=i,s.renderBufferDirect(d,null,b,S,F,null),s.copyFramebufferToTexture(n,y),L=k.uniforms,L.scale.value=r,L.screenPosition.value=i,s.renderBufferDirect(d,null,b,k,F,null);const j=-i.x*2,G=-i.y*2;for(let C=0,H=p.length;C<H;C++){const E=p[C],M=h.uniforms;M.color.value.copy(E.color),M.map.value=E.texture,M.screenPosition.value.x=i.x+j*E.distance,M.screenPosition.value.y=i.y+G*E.distance,V=E.size/t.w;const K=t.w/t.z;M.scale.value.set(V*K,V),h.uniformsNeedUpdate=!0,s.renderBufferDirect(d,null,b,h,o,null)}}},this.dispose=function(){S.dispose(),k.dispose(),h.dispose(),e.dispose(),y.dispose();for(let s=0,_=p.length;s<_;s++)p[s].texture.dispose()}}}class D{constructor(i,c=1,e=0,y=new W(16777215)){this.texture=i,this.size=c,this.distance=e,this.color=y}}D.Shader={uniforms:{map:{value:null},occlusionMap:{value:null},color:{value:null},scale:{value:null},screenPosition:{value:null}},vertexShader:`
+
+		precision highp float;
+
+		uniform vec3 screenPosition;
+		uniform vec2 scale;
+
+		uniform sampler2D occlusionMap;
+
+		attribute vec3 position;
+		attribute vec2 uv;
+
+		varying vec2 vUV;
+		varying float vVisibility;
+
+		void main() {
+
+			vUV = uv;
+
+			vec2 pos = position.xy;
+
+			vec4 visibility = texture2D( occlusionMap, vec2( 0.1, 0.1 ) );
+			visibility += texture2D( occlusionMap, vec2( 0.5, 0.1 ) );
+			visibility += texture2D( occlusionMap, vec2( 0.9, 0.1 ) );
+			visibility += texture2D( occlusionMap, vec2( 0.9, 0.5 ) );
+			visibility += texture2D( occlusionMap, vec2( 0.9, 0.9 ) );
+			visibility += texture2D( occlusionMap, vec2( 0.5, 0.9 ) );
+			visibility += texture2D( occlusionMap, vec2( 0.1, 0.9 ) );
+			visibility += texture2D( occlusionMap, vec2( 0.1, 0.5 ) );
+			visibility += texture2D( occlusionMap, vec2( 0.5, 0.5 ) );
+
+			vVisibility =        visibility.r / 9.0;
+			vVisibility *= 1.0 - visibility.g / 9.0;
+			vVisibility *=       visibility.b / 9.0;
+
+			gl_Position = vec4( ( pos * scale + screenPosition.xy ).xy, screenPosition.z, 1.0 );
+
+		}`,fragmentShader:`
+
+		precision highp float;
+
+		uniform sampler2D map;
+		uniform vec3 color;
+
+		varying vec2 vUV;
+		varying float vVisibility;
+
+		void main() {
+
+			vec4 texture = texture2D( map, vUV );
+			texture.a *= vVisibility;
+			gl_FragColor = texture;
+			gl_FragColor.rgb *= color;
+
+		}`};U.Geometry=function(){const w=new re,i=new Float32Array([-1,-1,0,0,0,1,-1,0,1,0,1,1,0,1,1,-1,1,0,0,1]),c=new ce(i,5);return w.setIndex([0,1,2,0,2,3]),w.setAttribute("position",new J(c,3,0,!1)),w.setAttribute("uv",new J(c,2,3,!1)),w}();const Fe="/nova/assets/lensflare0-BADikF6S.png",_e="/nova/assets/lensflare3-C9_pX5j7.png",ze={class:"viewCon"},m=600,Re=200,Ae=he({__name:"LensFlares",setup(w){const{random:i,PI:c,floor:e,ceil:y,min:b,max:S,sin:k,cos:F}=Math,p=O(),x=O();let h=window.innerWidth,o=window.innerHeight,r,n,u,t,s,_,d,g,A;const v={color:623843,intensity:1,distance:m,decay:.4};ue(async()=>{A=await G(Ee),V(),M(),H(),E(),q()});function V(){const l=new oe().addFolder("lfParams");l.open(),l.addColor(v,"color").onChange(a=>{d.color.setHex(a)}),l.add(v,"intensity",0,3,.1).onChange(a=>{d.intensity=a}),l.add(v,"distance",0,m*2,1).onChange(a=>{d.distance=a}),l.add(v,"decay",0,10,.1).onChange(a=>{d.decay=a})}const L=new Se;function j(){const f=L.getDelta();n.update(f)}function G(f){const l=new Le;return new Promise((a,z)=>{l.load(f,T=>{a(T)},void 0,T=>{z(T)})})}function C(f,l){const a=new Me(f,{font:A,size:20,height:1,curveSegments:1,bevelEnabled:!0,bevelThickness:2,bevelSize:2.4,bevelSegments:3});return new B(a,l)}function H(){const l="can't find a jod,None of this is going well,Demons get out of here".split(",");_=new de,u.add(_);const a=[new Z({color:16777215,flatShading:!0}),new Z({color:16777215})];for(let z=0,T=l.length;z<Re;z++){const P=C(l[z%T],a);P.position.x=i()*2*m-m,P.position.y=i()*2*m-m,P.position.z=i()*2*m-m,P.rotation.x=i()*c,P.rotation.y=i()*c,P.rotation.z=i()*c,_.add(P)}}function E(){d=new me(v.color,v.intensity,v.distance,v.decay),g=new U;const f=new ve,l=f.load(Fe),a=f.load(_e);g.addElement(new D(l,700,0,new W().setHex(v.color))),g.addElement(new D(a,60,.6)),g.addElement(new D(a,70,.7)),g.addElement(new D(a,120,.9)),g.addElement(new D(a,70,1)),d.add(g),u.add(d)}function M(){u=new pe,u.background=new W().setHSL(.51,.4,.01),u.fog=new fe(u.background,1,m*2),t=new we(75,h/o,1,m*2),t.position.set(m/2,m/2,m/2),t.lookAt(0,0,0),s=new ye({canvas:p.value,antialias:!0}),s.setSize(h,o),window.addEventListener("resize",te),r=Ve(),x.value.append(r.dom),n=new De(t,s.domElement),n.movementSpeed=100,n.domElement=s.domElement,n.rollSpeed=Math.PI/18,n.autoForward=!1,n.dragToLook=!1}let K=0;function q(){K=requestAnimationFrame(q),r.update(),j(),s.render(u,t)}be(()=>{cancelAnimationFrame(K)});function te(){h=window.innerWidth,o=window.innerHeight,t.aspect=h/o,t.updateProjectionMatrix(),s.setSize(h,o)}return(f,l)=>(xe(),ge("div",ze,[$("div",{ref_key:"canvasCon",ref:x,class:"canvasCon"},[$("canvas",{ref_key:"canvasDom",ref:p,class:"canvas"},null,512)],512)]))}}),Be=ke(Ae,[["__scopeId","data-v-93774b5d"]]);export{Be as default};
---
+++ docs/assets/index-sJn82dzZ.js
@@ -0,0 +1,139 @@
+import{d as v,r as f,h as p,V as u,aG as h,a8 as m,M as d,b as x,o as g}from"./index-DzmhKhXa.js";import{i as y}from"./initThree-PPOsCKF4.js";import"./OrbitControls-BHNdV32M.js";import"./initStats-C5VNgmNK.js";import"./_commonjsHelpers-BosuxZz1.js";const b=`varying vec2 vUv;
+
+void main() {
+  vUv = uv;
+  gl_Position = vec4(position, 1.0);
+}`,_=`precision mediump float;\r
+\r
+uniform vec2 iResolution;\r
+uniform float iTime;\r
+uniform sampler2D texture1;\r
+uniform float size;\r
+\r
+// https://www.shadertoy.com/view/4dS3Wd\r
+float hash(float p) {\r
+  p = fract(p * 0.011);\r
+  p *= p + 7.5;\r
+  p *= p + p;\r
+  return fract(p);\r
+}\r
+float hash(vec2 p) {\r
+  vec3 p3 = fract(vec3(p.xyx) * 0.13);\r
+  p3 += dot(p3, p3.yzx + 3.333);\r
+  return fract((p3.x + p3.y) * p3.z);\r
+}\r
+\r
+float noise(vec2 x) {\r
+  vec2 i = floor(x);\r
+  vec2 f = fract(x);\r
+\r
+	// Four corners in 2D of a tile\r
+  float a = hash(i);\r
+  float b = hash(i + vec2(1.0, 0.0));\r
+  float c = hash(i + vec2(0.0, 1.0));\r
+  float d = hash(i + vec2(1.0, 1.0));\r
+\r
+    // Simple 2D lerp using smoothstep envelope between the values.\r
+	// return vec3(mix(mix(a, b, smoothstep(0.0, 1.0, f.x)),\r
+	//			mix(c, d, smoothstep(0.0, 1.0, f.x)),\r
+	//			smoothstep(0.0, 1.0, f.y)));\r
+\r
+	// Same code, with the clamps in smoothstep and common subexpressions\r
+	// optimized away.\r
+  vec2 u = f * f * (3.0 - 2.0 * f);\r
+  return mix(a, b, u.x) + (c - a) * u.y * (1.0 - u.x) + (d - b) * u.x * u.y;\r
+}\r
+float noise(vec3 x) {\r
+  const vec3 step = vec3(110, 241, 171);\r
+\r
+  vec3 i = floor(x);\r
+  vec3 f = fract(x);\r
+\r
+    // For performance, compute the base input to a 1D hash from the integer part of the argument and the \r
+    // incremental change to the 1D based on the 3D -> 1D wrapping\r
+  float n = dot(i, step);\r
+\r
+  vec3 u = f * f * (3.0 - 2.0 * f);\r
+  return mix(mix(mix(hash(n + dot(step, vec3(0, 0, 0))), hash(n + dot(step, vec3(1, 0, 0))), u.x), mix(hash(n + dot(step, vec3(0, 1, 0))), hash(n + dot(step, vec3(1, 1, 0))), u.x), u.y), mix(mix(hash(n + dot(step, vec3(0, 0, 1))), hash(n + dot(step, vec3(1, 0, 1))), u.x), mix(hash(n + dot(step, vec3(0, 1, 1))), hash(n + dot(step, vec3(1, 1, 1))), u.x), u.y), u.z);\r
+}\r
+\r
+vec3 palette(float t) {\r
+  vec3 a = vec3(0.731, 1.098, 0.192);\r
+  vec3 b = vec3(0.358, 1.090, 0.657);\r
+  vec3 c = vec3(1.077, 0.360, 0.328);\r
+  vec3 d = vec3(0.965, 2.265, 0.837);\r
+  return a + b * cos(6.28318 * (c * t + d));\r
+}\r
+float PI = 3.1415926;\r
+float lineWidth;\r
+\r
+// p屏幕坐标，a起点，b终点\r
+float sdSegment(in vec2 p, in vec2 a, in vec2 b) {\r
+  vec2 pa = p - a, ba = b - a;\r
+  float h = clamp(dot(pa, ba) / dot(ba, ba), 0.0, 1.0);\r
+  return length(pa - ba * h);\r
+}\r
+float plotLine(in vec2 uv,in vec2 start,in vec2 end,in float lineWidth) {\r
+  float l = sdSegment(uv, start, end) - lineWidth;\r
+  return smoothstep(lineWidth * 1.5, lineWidth, abs(l));\r
+}\r
+\r
+float plotPoint(vec2 uv, vec2 pos) {\r
+    float r = .2;\r
+    float point = distance(uv, pos) - r;\r
+    return smoothstep(0.1, 0., point);\r
+}\r
+void main() {\r
+  float resolution = 20.;\r
+  \r
+  // 非常重要，坐标连续化的前提，原理是2+1=3\r
+  // 把断裂的坐标拼接起来，前提是uv是归一化的，\r
+  // 比如第1行第8列到第1行第9列，x坐标是8-9,9-10，这样两个格子的x坐标就变成连续的了\r
+  vec2 uv = gl_FragCoord.xy / iResolution.y * resolution; \r
+  lineWidth = 0.05;\r
+\r
+  vec2 uvi = floor(uv);\r
+  vec2 uvf = fract(uv);\r
+  vec3 fin_c = vec3(0.);\r
+\r
+  float lineLen = 1.5;\r
+  \r
+  float range = ceil(lineLen);\r
+\r
+  if(false) {\r
+      // 仅仅是在当前cell中绘制了线段\r
+      // 如果线段长度超出1.（预设的单元尺寸，在这里是1），就会不去绘制\r
+      // 因为在其他单元中是绘制对应他uvi坐标对应的线\r
+      // 所以一条完整的线，应该除了自身单元需要绘制，还有周边range*range-1个单元需要叠加绘制\r
+      float noiseVal = noise(uvi / resolution + iTime) * 0.5 + 0.5;\r
+      float angle = noiseVal * PI * 2.;\r
+      vec2 start = vec2(0.5) + uvi;\r
+      vec2 end = vec2(cos(angle) * lineLen, sin(angle) * lineLen) + start;\r
+      float line = plotLine(uv, start, end, lineWidth);\r
+      vec3 color = palette(noiseVal);\r
+      fin_c += color * line;\r
+  } else {\r
+      // 循环range*range多层绘制出完整的线段\r
+      for(float x = -range; x <= range; x += 1.) {\r
+          for(float y = -range; y <= range; y += 1.) {\r
+          \r
+              // 对当前单元来说，线的起点在反方向，需要-vec2(x,y)\r
+              vec2 start = vec2(0.5) + uvi - vec2(x,y);\r
+              \r
+              float noiseVal = noise((uvi-vec2(x,y)) / resolution + iTime) * 0.5 + 0.5;\r
+              float angle = noiseVal * PI * 2.;\r
+              vec3 color = palette(noiseVal);\r
+              //lineLen *= noiseVal + 0.1;\r
+\r
+              vec2 end = vec2(cos(angle) * lineLen, sin(angle) * lineLen) + start;\r
+              float line = plotLine(uv, start, end, lineWidth);\r
+\r
+              fin_c += color * line;\r
+          }\r
+      }\r
+  \r
+  }\r
+\r
+  \r
+  gl_FragColor = vec4(fin_c, 1.);\r
+}`,P=v({__name:"index",setup(w){const n=f();return p(()=>{const{scene:e,renderWrap:r}=y(n.value),{width:a,height:i}=n.value.getBoundingClientRect(),t={iResolution:{value:new u(a,i)},iTime:{value:0}},l=new h({uniforms:t,vertexShader:b,fragmentShader:_}),s=new m(1e3,1e3),o=new d(s,l);o.position.set(0,0,10),e.add(o),r(()=>{const c=performance.now()/1e3;t.iTime.value=c})()}),(e,r)=>(g(),x("div",{ref_key:"con",ref:n,class:"w-full h-full relative"},null,512))}});export{P as default};
