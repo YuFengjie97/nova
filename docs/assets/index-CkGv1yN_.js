e334aaad7b8df3d9482cedae1f49137fd1c513c1
yufengjie <2457522528@qq.com>
2025-02-16 13:46:16 +0800

    'build'

---
+++ docs/assets/index-CkGv1yN_.js
@@ -0,0 +1,60 @@
+import{_ as r}from"./Shader.vue_vue_type_script_setup_true_lang-BtEn2PT7.js";import{d as n,e as o,q as t,A as e,o as a}from"./index-CXaHQ9wk.js";const l=`precision mediump float;\r
+\r
+#define PI 3.1415926\r
+uniform vec2 iResolution;\r
+uniform float iTime;\r
+\r
+vec3 palette(float t) {\r
+  vec3 a = vec3(0.731, 1.098, 0.192);\r
+  vec3 b = vec3(0.358, 1.090, 0.657);\r
+  vec3 c = vec3(1.077, 0.360, 0.328);\r
+  vec3 d = vec3(0.965, 2.265, 0.837);\r
+  return a + b * cos(6.28318 * (c * t + d));\r
+}\r
+\r
+float randomFloat(float seed) {\r
+  return fract(sin(seed * 13.14));\r
+}\r
+\r
+vec2 random(float seed) {\r
+  float x = fract(sin(seed * 12123.321));\r
+  float y = fract(sin(seed * 72353.456));\r
+  return vec2(x, y);\r
+}\r
+\r
+float plotGlowParticle(vec2 uv, vec2 pos, float r, float glowFactor) {\r
+  float v = length(uv - pos) - r;\r
+  //return smoothstep(0.01, 0.,abs(v));\r
+  return exp(-1. * glowFactor * v);\r
+}\r
+\r
+void main() {\r
+  vec2 uv = (gl_FragCoord.xy * 2. - iResolution.xy) / iResolution.y;\r
+  float ixy = iResolution.x / iResolution.y;\r
+  vec3 c_fin = vec3(0.);\r
+  float pix = 1. / iResolution.y;\r
+\r
+  float total = 100.;\r
+\r
+  for(float i = 0.; i < total; i += 1.) {\r
+\r
+    float factor = i / total;\r
+    float radius = (10. * factor + 10.) * pix;\r
+    float glowFactor = 10. + (1. - i / total) * 40.;\r
+    // float isClock = randomFloat(i) > 0.5 ? 1. : -1.;\r
+    float isClock = -1.;\r
+    float speed = (10. * factor + 1.) * 0.1;\r
+\r
+    vec2 trackCenter = random(i) * vec2(ixy * 2., 2.);\r
+    float trackRadius = randomFloat(i) * .8 + .1;\r
+    vec2 pos = trackCenter + vec2(cos(iTime * speed), sin(iTime * speed)) * trackRadius * isClock;\r
+\r
+    float glowParticle = plotGlowParticle(uv, pos, radius, glowFactor);\r
+\r
+    //c_fin = mix(c_fin, palette(factor), glowParticle);\r
+    c_fin += palette(factor) * glowParticle;\r
+  }\r
+\r
+  gl_FragColor = vec4(c_fin, 1.);\r
+}\r
+`,c={class:"w-full h-full"},u=n({__name:"index",setup(i){return(s,f)=>(a(),o("div",c,[t(r,{fs:e(l)},null,8,["fs"])]))}});export{u as default};
---
+++ docs/assets/index-wlbI2k64.js
@@ -0,0 +1,73 @@
+import{_ as r}from"./Shader.vue_vue_type_script_setup_true_lang-BtEn2PT7.js";import{d as n,e,q as o,A as t,o as i}from"./index-CXaHQ9wk.js";const c=`#define resolution 30.\r
+#define r_particle 0.1\r
+#define PI 3.141592654\r
+\r
+precision mediump float;\r
+uniform vec2 iResolution;\r
+uniform float iTime;\r
+uniform vec2 iMouse;\r
+\r
+float pix;\r
+\r
+float plotCircle(vec2 p, vec2 c, float r) {\r
+    float cir = length(p-c) - r;\r
+    return smoothstep(pix*2., 0., cir);\r
+}\r
+\r
+vec3 palette(float t) {\r
+  vec3 a = vec3(0.731, 1.098, 0.192);\r
+  vec3 b = vec3(0.358, 1.090, 0.657);\r
+  vec3 c = vec3(1.077, 0.360, 0.328);\r
+  vec3 d = vec3(0.965, 2.265, 0.837);\r
+  return a + b * cos(6.28318 * (c * t + d));\r
+}\r
+\r
+void main()\r
+{\r
+    vec2 uv = gl_FragCoord.xy/iResolution.y * resolution;\r
+    float ixy = iResolution.x / iResolution.y;\r
+    pix = 1. / iResolution.y * resolution;\r
+    vec3 c_fin = vec3(0.);\r
+\r
+    vec2 uvi = floor(uv);\r
+    vec2 uvf = fract(uv);\r
+    \r
+    float dist_max = resolution*2.;\r
+    float off_max = 4.6;\r
+    float range = ceil(off_max + r_particle);\r
+    \r
+    #if 0\r
+        float b1 = smoothstep(2. * pix, 0., abs(uvf.x));\r
+        float b2 = smoothstep(2. * pix, 0., abs(uvf.y));\r
+        float b3 = smoothstep(2. * pix, 0., abs(uvf.x - 1.));\r
+        float b4 = smoothstep(2. * pix, 0., abs(uvf.y - 1.));\r
+        float b = b1 + b2 + b3 + b4;\r
+        c_fin += b;\r
+    #endif\r
+    \r
+    vec2 target = vec2((cos(iTime * 0.5) * 0.5 + 0.5) * resolution * ixy, (sin(iTime) * 0.5 + 0.5) * resolution);\r
+    if(iMouse.x != 0.) {\r
+        target = iMouse.xy / iResolution.y * resolution;\r
+    }\r
+    float cc = plotCircle(uv, target, 0.5);\r
+    c_fin += cc;\r
+    \r
+    for(float x = -range; x <= range; x += 1.) {\r
+        for(float y = -range; y <= range; y += 1.) {\r
+            vec2 mc = uvi + vec2(x,y) + vec2(0.5) - target;\r
+            vec2 mcn = normalize(mc);\r
+            float dist = length(mc);\r
+            float d = clamp(0., 1., dist / dist_max);\r
+            vec2 off = mcn * (1. - d) * off_max;\r
+\r
+            vec3 color = palette(d);\r
+            float cir = plotCircle(uv, uvi + vec2(x,y) + vec2(0.5) + off, r_particle);\r
+            c_fin = mix(c_fin, color, cir);\r
+            //c_fin += cir * color;\r
+\r
+        }\r
+    }\r
+    \r
+\r
+    gl_FragColor = vec4(c_fin,1.0);\r
+}`,f={class:"w-full h-full"},m=n({__name:"index",setup(l){return(a,s)=>(i(),e("div",f,[o(r,{fs:t(c)},null,8,["fs"])]))}});export{m as default};
